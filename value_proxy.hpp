00ed68434118029a31137e3c003a3789922b6098
Mario Lang <mlang@delysid.org>
2011-07-07 00:23:04 +0200

    kill dead code

--- value_proxy.hpp
+++ value_proxy.hpp
@@ -64,11 +64,7 @@ public:
 
 class value_proxy_list
 : public std::vector< std::vector< std::vector<value_proxy> > >
-, public boost::static_visitor<void>
 {
-  enum choices { just_large, just_small, both };
-  choices choice;
-  ambiguous::value last_value;
 public:
   class large_follows : public boost::static_visitor<bool>
   {
@@ -239,7 +235,6 @@ public:
   }
   value_proxy_list(ambiguous::partial_measure_voice& voice)
   : std::vector< std::vector< std::vector<value_proxy> > >()
-  , choice(both), last_value(ambiguous::unknown)
   {
     ambiguous::partial_measure_voice::iterator begin = voice.begin();
     while (begin != voice.end()) {
@@ -262,54 +257,6 @@ public:
       }
       std::advance(begin, std::distance(begin, end));
     }
-  }
-  template<class Note>
-  void operator()(Note& note) {
-    value_type possibilities;
-    if (choice == just_large) {
-      if (last_value == ambiguous::unknown) last_value = value(note);
-      if (last_value == value(note)) {
-        possibilities.push_back(value_type::value_type(1, value_proxy(note, large)));
-      } else {
-        last_value = ambiguous::unknown;
-        choice = both;
-      }
-    } else if (choice == just_small) {
-      if (last_value == ambiguous::unknown) last_value = value(note);
-      if (last_value == value(note)) {
-        possibilities.push_back(value_type::value_type(1, value_proxy(note, small)));
-      } else {
-        last_value = ambiguous::unknown;
-        choice = both;
-      }
-    }
-    if (choice == both) {
-      possibilities.push_back(value_type::value_type(1, value_proxy(note, large)));
-      possibilities.push_back(value_type::value_type(1, value_proxy(note, small)));
-    }
-    BOOST_ASSERT(possibilities.size() <= 2);
-    push_back(possibilities);
-  }
-  void operator()(ambiguous::value_distinction value_distinction) {
-    switch (value_distinction) {
-    case ambiguous::large_follows:
-      choice = just_large;
-      break;
-    case ambiguous::small_follows:
-      choice = just_small;
-      break;
-    default: BOOST_ASSERT(false);
-    }
-  }
-  void operator()(ambiguous::simile&) {
-  }
-private:
-  template<class NoteOrRest>
-  static ambiguous::value value(NoteOrRest& note) {
-    return note.ambiguous_value;
-  }
-  static ambiguous::value value(ambiguous::chord& chord) {
-    return value(chord.base);
   }
 };
 
