a998e72fca7bade5bf0343789a57d5d247fe57cf
Mario Lang <mlang@delysid.org>
2011-08-11 14:31:46 +0200

    Explicitly declare our license

--- brltr.cpp
+++ brltr.cpp
@@ -1,3 +1,9 @@
+// Copyright (C) 2011  Mario Lang <mlang@delysid.org>
+//
+// Distributed under the terms of the GNU General Public License version 3.
+// (see accompanying file LICENSE.txt or copy at
+//  http://www.gnu.org/licenses/gpl-3.0-standalone.html)
+
 #include <fstream>
 #include <iostream>
 #include <locale>
--- numbers.hpp
+++ numbers.hpp
@@ -1,3 +1,9 @@
+// Copyright (C) 2011  Mario Lang <mlang@delysid.org>
+//
+// Distributed under the terms of the GNU General Public License version 3.
+// (see accompanying file LICENSE.txt or copy at
+//  http://www.gnu.org/licenses/gpl-3.0-standalone.html)
+
 #if !defined(BMC_NUMBERS_HPP)
 #define BMC_NUMBERS_HPP
 
