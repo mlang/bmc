8c804431da11ac9b087a67a4a8a80f7bf1aef633
Mario Lang <mlang@delysid.org>
2011-09-01 18:36:14 +0200

    Simplify.

--- spirit_info_wchar_t_io.hpp
+++ spirit_info_wchar_t_io.hpp
@@ -7,28 +7,31 @@
 #include <boost/spirit/home/support/info.hpp>
 #include <boost/locale/encoding_utf.hpp>
 
+// See https://svn.boost.org/trac/boost/ticket/5716
+
 namespace boost { namespace spirit {
 
-template <>
-struct simple_printer < std::basic_ostream<wchar_t> >
+template<>
+struct simple_printer<std::wostream>
 {
   typedef utf8_string string;
 
-  simple_printer(std::basic_ostream<wchar_t>& out) : out(out) {}
+  simple_printer(std::wostream& out) : out(out) {}
 
   void element(string const& tag, string const& value, int /*depth*/) const
   {
+    typedef std::wostream::char_type char_type;
     if (value == "")
-      out << wchar_t('<')
-          << locale::conv::utf_to_utf<wchar_t>(tag)
-          << wchar_t('>');
+      out << char_type('<')
+          << locale::conv::utf_to_utf<char_type>(tag)
+          << char_type('>');
     else
-      out << wchar_t('"')
-          << locale::conv::utf_to_utf<wchar_t>(value)
-          << wchar_t('"');
+      out << char_type('"')
+          << locale::conv::utf_to_utf<char_type>(value)
+          << char_type('"');
   }
 
-  std::basic_ostream<wchar_t>& out;
+  std::wostream& out;
 };
 
 }}
