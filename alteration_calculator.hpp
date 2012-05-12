2af7e7db1b7cc97a558bd4b37acf98dc8d03f4b0
Mario Lang <mlang@delysid.org>
2012-05-13 00:32:46 +0200

    Support double_flat and double_sharp key signatures.

--- alteration_calculator.hpp
+++ alteration_calculator.hpp
@@ -31,6 +31,27 @@ class alteration_calculator
 {
   music::accidental memory[10][steps_per_octave];
   music::key_signature key_sig;
+
+  struct signmap: std::multimap<rational const, ambiguous::sign&>
+  {
+    signmap(ambiguous::measure& measure, rational const& measure_position = rational())
+    {
+      for (ambiguous::voice& voice: measure.voices) {
+        rational voice_position(measure_position);
+        for (ambiguous::partial_measure& partial_measure: voice) {
+          for (ambiguous::partial_voice& partial_voice: partial_measure) {
+            rational position(voice_position);
+            for (ambiguous::sign& sign: partial_voice) {
+              insert(value_type(position, sign));
+              position += duration(sign);
+            }
+          }
+          voice_position += duration(partial_measure);
+        }
+      }
+    }
+  };
+
 public:
   alteration_calculator(report_error_type const& report_error)
   : compiler_pass(report_error)
@@ -42,29 +63,12 @@ public:
   void set(music::key_signature sig)
   { key_sig = sig; }
 
-  result_type operator()(ambiguous::measure& measure)
+  result_type operator() (ambiguous::measure& measure)
   {
     reset_memory(key_sig);
 
-    // Flatten and sort all elements along the time axis
-    typedef std::multimap<rational, ambiguous::sign&> eventmap;
-    eventmap map;
-    for (ambiguous::voice& voice: measure.voices) {
-      rational voice_position;
-      for (ambiguous::partial_measure& partial_measure: voice) {
-        for (ambiguous::partial_voice& partial_voice: partial_measure) {
-          rational position(voice_position);
-          for (ambiguous::sign& sign: partial_voice) {
-            map.insert(eventmap::value_type(position, sign));
-            position += duration(sign);
-          }
-        }
-        voice_position += duration(partial_measure);
-      }
-    }
-
     // Visit all elements of this measure in chronological order
-    for (auto& value: map) boost::apply_visitor(*this, value.second);
+    for (auto& value: signmap(measure)) boost::apply_visitor(*this, value.second);
   }
 
   result_type operator()(ambiguous::note& note)
@@ -107,22 +111,41 @@ private:
     for (std::size_t octave = 0; octave < 10; ++octave) {
       for (std::size_t step = C; step < steps_per_octave; ++step)
         memory[octave][step] = natural;
+      int count = steps_per_octave;
       switch (key_signature) {
-      case  7: memory[octave][B] = sharp;
-      case  6: memory[octave][E] = sharp;
-      case  5: memory[octave][A] = sharp;
-      case  4: memory[octave][D] = sharp;
-      case  3: memory[octave][G] = sharp;
-      case  2: memory[octave][C] = sharp;
-      case  1: memory[octave][F] = sharp;
-      case  0: break;
-      case -7: memory[octave][F] = flat;
-      case -6: memory[octave][C] = flat;
-      case -5: memory[octave][G] = flat;
-      case -4: memory[octave][D] = flat;
-      case -3: memory[octave][A] = flat;
-      case -2: memory[octave][E] = flat;
-      case -1: memory[octave][B] = flat;
+      case  14: memory[octave][B] = double_sharp; if (not --count) break;
+      case  13: memory[octave][E] = double_sharp; if (not --count) break;
+      case  12: memory[octave][A] = double_sharp; if (not --count) break;
+      case  11: memory[octave][D] = double_sharp; if (not --count) break;
+      case  10: memory[octave][G] = double_sharp; if (not --count) break;
+      case   9: memory[octave][C] = double_sharp; if (not --count) break;
+      case   8: memory[octave][F] = double_sharp; if (not --count) break;
+
+      case   7: memory[octave][B] = sharp;        if (not --count) break;
+      case   6: memory[octave][E] = sharp;        if (not --count) break;
+      case   5: memory[octave][A] = sharp;        if (not --count) break;
+      case   4: memory[octave][D] = sharp;        if (not --count) break;
+      case   3: memory[octave][G] = sharp;        if (not --count) break;
+      case   2: memory[octave][C] = sharp;        if (not --count) break;
+      case   1: memory[octave][F] = sharp;        if (not --count) break;
+
+      case   0: break;
+
+      case -14: memory[octave][F] = double_flat; if (not --count) break;
+      case -13: memory[octave][C] = double_flat; if (not --count) break;
+      case -12: memory[octave][G] = double_flat; if (not --count) break;
+      case -11: memory[octave][D] = double_flat; if (not --count) break;
+      case -10: memory[octave][A] = double_flat; if (not --count) break;
+      case  -9: memory[octave][E] = double_flat; if (not --count) break;
+      case  -8: memory[octave][B] = double_flat; if (not --count) break;
+
+      case  -7: memory[octave][F] = flat;        if (not --count) break;
+      case  -6: memory[octave][C] = flat;        if (not --count) break;
+      case  -5: memory[octave][G] = flat;        if (not --count) break;
+      case  -4: memory[octave][D] = flat;        if (not --count) break;
+      case  -3: memory[octave][A] = flat;        if (not --count) break;
+      case  -2: memory[octave][E] = flat;        if (not --count) break;
+      case  -1: memory[octave][B] = flat;        if (not --count) break;
       }
     }
   }
