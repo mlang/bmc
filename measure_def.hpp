9839b9deed2888e39e392dbb2410da8a870a61a0
Mario Lang <mlang@debian.org>
2012-10-05 00:03:46 +0200

    More granular include.

--- measure_def.hpp
+++ measure_def.hpp
@@ -21,7 +21,7 @@
 #include <boost/spirit/include/phoenix_function.hpp>
 #include <boost/spirit/include/phoenix_fusion.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_statement.hpp>
+#include <boost/phoenix/statement/sequence.hpp>
 #include <boost/fusion/include/std_pair.hpp>
 
 namespace music { namespace braille {
