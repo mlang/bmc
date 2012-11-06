42f872a6d13666d1a428f0fe4f1bcb22ca8842c2
Mario Lang <mlang@debian.org>
2012-11-06 02:55:34 +0100

    Split out LilyPond export into its own frontend, bmc2ly.

---
+++ bmc2ly.cpp
@@ -0,0 +1,56 @@
+// Copyright (C) 2011-2012  Mario Lang <mlang@delysid.org>
+//
+// Distributed under the terms of the GNU General Public License version 3.
+// (see accompanying file LICENSE.txt or copy at
+//  http://www.gnu.org/licenses/gpl-3.0-standalone.html)
+
+#include <bmc/ttb/ttb.h>
+
+#include "config.hpp"
+#include <boost/spirit/include/qi_parse.hpp>
+#include "bmc/braille/parsing/grammar/score.hpp"
+#include "bmc/braille/semantic_analysis.hpp"
+
+#include "bmc/lilypond.hpp"
+
+int
+main(int argc, char const *argv[])
+{
+  std::locale::global(std::locale(""));
+
+  {
+    char *localeTable = selectTextTable(TABLES_DIRECTORY);
+    if (localeTable) {
+      replaceTextTable(TABLES_DIRECTORY, localeTable);
+      free(localeTable);
+    }
+  }
+
+  std::istreambuf_iterator<wchar_t> wcin_begin(std::wcin.rdbuf()), wcin_end;
+  std::wstring source(wcin_begin, wcin_end);
+  typedef std::wstring::const_iterator iterator_type;
+
+  iterator_type iter = source.begin();
+  iterator_type const end = source.end();
+  typedef music::braille::error_handler<iterator_type> error_handler_type;
+  error_handler_type error_handler(iter, end);
+  typedef music::braille::score_grammar<iterator_type> parser_type;
+  parser_type parser(error_handler);
+  boost::spirit::traits::attribute_of<parser_type>::type score;
+
+  bool const success = parse(iter, end, parser, score);
+
+  if (success and iter == end) {
+    music::braille::compiler<error_handler_type> compile(error_handler);
+    if (compile(score)) {
+      music::lilypond_output_format(std::cout);
+      //music::include_locations_for_lilypond(std::cout);
+      std::cout << score;
+
+      return EXIT_SUCCESS;
+    }
+  }
+
+  return EXIT_FAILURE;
+}
+
