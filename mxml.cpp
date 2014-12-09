9ba20a9b75b14d68df4b0fee623453a21d8f13b1
Mario Lang <mlang@delysid.org>
2014-12-09 13:21:55 +0100

    Note, chord, and rest skeletons.

--- mxml.cpp
+++ mxml.cpp
@@ -85,88 +85,145 @@ class make_measures_for_staff_visitor : public boost::static_visitor<void> {
   unsigned staff_number;
   rational divisions;
   unsigned measure_number = 1;
+  measure_type *current_measure;
 public:
   make_measures_for_staff_visitor(part_type::measure_sequence &measures,
                                   unsigned staff_number,
                                   rational const &divisions)
-  : measures { measures }, staff_number { staff_number }, divisions { divisions } {}
+  : measures { measures }, staff_number { staff_number }, divisions { divisions }
+  , current_measure { nullptr }
+  {}
 
   void operator()(braille::ast::unfolded::measure const &measure) {
     if (measure_number > measures.size())
       measures.push_back({std::to_string(measure_number)});
 
-    measure_type &xml_measure = measures[measure_number - 1];
+    current_measure = &measures[measure_number - 1];
 
     if (staff_number > 1)
-      ::musicxml::push_back(xml_measure, backup(duration(measure), divisions));
+      ::musicxml::push_back(*current_measure, backup(duration(measure), divisions));
     for (auto vi = measure.voices.begin(), ve = measure.voices.end();
          vi != ve; ++vi) {
       for (auto &&partial_measure: *vi) {
         for (auto pvi = partial_measure.begin(), pve = partial_measure.end();
              pvi != pve; ++pvi) {
+          std::for_each(pvi->begin(), pvi->end(), apply_visitor(*this));
           if (std::next(pvi) != pve)
-            ::musicxml::push_back(xml_measure, backup(duration(*pvi), divisions));
+            ::musicxml::push_back(*current_measure, backup(duration(*pvi), divisions));
         }
       }
       if (std::next(vi) != ve)
-        ::musicxml::push_back(xml_measure, backup(duration(*vi), divisions));
+        ::musicxml::push_back(*current_measure, backup(duration(*vi), divisions));
     }
 
     measure_number += 1;
   }
   void operator()(braille::ast::key_and_time_signature const &) {
   }
+  void operator()(braille::ast::note const &note) const {
+    ::musicxml::note xml_note { };
+    xml_note.duration(boost::rational_cast<double>(note.as_rational() / (rational{1, 4} / divisions)));
+    ::musicxml::push_back(*current_measure, xml_note);
+  }
+  void operator()(braille::ast::rest const &rest) const {
+    ::musicxml::note xml_note { };
+    xml_note.rest(::musicxml::rest{});
+    xml_note.duration(boost::rational_cast<double>(rest.as_rational() / (rational{1, 4} / divisions)));
+    ::musicxml::push_back(*current_measure, xml_note);
+  }
+  void operator()(braille::ast::chord const &chord) const {
+    (*this)(chord.base);
+    for (auto &&interval: chord.intervals) {
+      ::musicxml::note xml_note { };
+      xml_note.chord(::musicxml::empty{});
+      ::musicxml::push_back(*current_measure, xml_note);
+    }
+  }
+  void operator()(braille::ast::moving_note const &moving_note) const {
+    (*this)(moving_note.base);
+    ::musicxml::push_back(*current_measure, backup(moving_note.base.as_rational(), divisions));
+    for (auto &&interval: moving_note.intervals) {
+      ::musicxml::note xml_note { };
+      xml_note.chord(::musicxml::empty{});
+      ::musicxml::push_back(*current_measure, xml_note);
+    }
+  }
+  void operator()(braille::ast::barline const &) const {
+  }
+  void operator()(braille::ast::clef const &) const {
+  }
+  void operator()(braille::hand_sign const &) const {
+  }
+  void operator()(braille::ast::tie const &) const {
+  }
 };
 
-part_type::measure_sequence get_measures(braille::ast::unfolded::part const &p, rational const &divisions) {
-  measure_type initial_measure { "1" };
-  ::musicxml::attributes attributes { };
-  attributes.divisions(boost::rational_cast<double>(divisions));
-  attributes.staves(p.size());
-  ::musicxml::push_back(initial_measure, attributes);
+class musicxml_generator {
+  braille::ast::score const &brl_score;
+  score_type xml_score;
+  ::musicxml::attributes global_attributes;
+  rational divisions;
 
-  part_type::measure_sequence measures;
-  measures.push_back(initial_measure);
+public:
+  musicxml_generator(braille::ast::score const &score)
+  : brl_score { score }, xml_score { ::musicxml::part_list {} }
+  , global_attributes { }
+    // The divisons element expresses the number of "ticks" per quarter note.
+  , divisions {
+      rational{1, 4} / boost::math::gcd(duration_gcd(brl_score), rational{1, 4})
+    }
+  {
+    BOOST_ASSERT(divisions.denominator() == 1);
 
-  unsigned staff_number { 1 };
-  for (auto &&staff: p) {
-    make_measures_for_staff_visitor visitor {
-      measures, staff_number, divisions
-    };
-    std::for_each(staff.begin(), staff.end(), apply_visitor(visitor));
+    global_attributes.divisions(boost::rational_cast<double>(divisions));
+    global_attributes.key().push_back(xml(brl_score.key_sig));
+    if (not brl_score.time_sigs.empty())
+      global_attributes.time().push_back(xml(brl_score.time_sigs.front()));
 
-    staff_number += 1;
+    unsigned c { 1 };
+    for (auto &&p: brl_score.unfolded_part) {
+      part_type part { "P" + std::to_string(c) };
+
+      part.measure(get_measures(p));
+
+      xml_score.part_list().score_part().push_back({
+        "Part-" + std::to_string(c++), part.id()
+      });
+      xml_score.part().push_back(part);
+    }
   }
 
-  return measures;
-}
+  part_type::measure_sequence get_measures(braille::ast::unfolded::part const &p)
+  {
+    measure_type initial_measure { "1" };
+    ::musicxml::attributes attributes { global_attributes };
+    attributes.staves(p.size());
+    ::musicxml::push_back(initial_measure, attributes);
+
+    part_type::measure_sequence measures;
+    measures.push_back(initial_measure);
+
+    unsigned staff_number { 1 };
+    for (auto &&staff: p) {
+      make_measures_for_staff_visitor visitor {
+        measures, staff_number, divisions
+      };
+      std::for_each(staff.begin(), staff.end(), apply_visitor(visitor));
+
+      staff_number += 1;
+    }
+
+    return measures;
+  }
+
+  score_type score_partwise() const { return xml_score; }
+};
 
 }
 
 void musicxml(std::ostream &os, music::braille::ast::score const &score)
 {
-  // The divisons element expresses the number of "ticks" per quarter note.
-  rational const divisions {
-    rational{1, 4} / boost::math::gcd(duration_gcd(score), rational{1, 4})
-  };
-  BOOST_ASSERT(divisions.denominator() == 1);
-
-  ::musicxml::attributes global_attributes {};
-  global_attributes.key().push_back(xml(score.key_sig));
-  std::clog << global_attributes;
-  score_type xml_score { ::musicxml::part_list {} };
-  unsigned c { 1 };
-  for (auto &&p: score.unfolded_part) {
-    part_type part { "P" + std::to_string(c) };
-
-    part.measure(get_measures(p, divisions));
-
-    xml_score.part_list().score_part().push_back({
-      "Part-" + std::to_string(c++), part.id()
-    });
-    xml_score.part().push_back(part);
-  }
-  ::musicxml::score_partwise_(os, xml_score);
+  ::musicxml::score_partwise_(os, musicxml_generator(score).score_partwise());
 }
 
 }
