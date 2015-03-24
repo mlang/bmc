03eafa7f1fd67e78da567de4a7bfcfbf1fa7ce6c
Mario Lang <mlang@delysid.org>
2015-03-24 20:07:21 +0100

    [Qt] Set application name and version.

--- ui/qt/app.cpp
+++ ui/qt/app.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QApplication::setApplicationName("Braille Music Compiler");
+    QApplication::setApplicationVersion(BMC_VERSION);
     BrailleMusicEditor window;
     window.show();
 
