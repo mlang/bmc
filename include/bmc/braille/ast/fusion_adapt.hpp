ff6dea55d1c090dbf747a0ef8975a8b1465991db
Mario Lang <mlang@delysid.org>
2016-03-12 18:22:30 +0100

    Simplify Fusion AST adaption.

--- include/bmc/braille/ast/fusion_adapt.hpp
+++ include/bmc/braille/ast/fusion_adapt.hpp
@@ -5,116 +5,77 @@
 #include <boost/fusion/include/std_pair.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::clef,
-  (::bmc::braille::ast::clef::type, sign)
-  (::boost::optional<unsigned>, line)
-  (bool, other_staff)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::clef,
+  sign, line, other_staff
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::slur,
-  (::bmc::braille::ast::slur::type, value)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::slur,
+  value
 )
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::tie,
-  (::bmc::braille::ast::tie::type, value)
+
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::tie,
+  value
 )
+
 BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::hand_sign,
-  (::bmc::braille::ast::hand_sign::type, value)
+  value
 )
+
 BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::rest,
-  (bool, by_transcriber)
-  (::bmc::braille::ast::value, ambiguous_value)
-  (unsigned, dots)
-)
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::stem,
-  (::bmc::rational, type)
-  (unsigned, dots)
-  (boost::optional<::bmc::braille::ast::tie>, tied)
-)
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::note,
-  (std::vector<::bmc::articulation>, articulations)
-  (boost::optional<::bmc::accidental>, acc)
-  (boost::optional<unsigned>, octave_spec)
-  (::bmc::diatonic_step, step)
-  (::bmc::braille::ast::value, ambiguous_value)
-  (unsigned, dots)
-  (std::vector<::bmc::braille::ast::slur>, slurs)
-  (::bmc::braille::fingering_list, fingers)
-  (boost::optional<::bmc::braille::ast::tie>, tie)
-  (std::vector<::bmc::braille::ast::stem>, extra_stems)
-)
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::interval,
-  (boost::optional<::bmc::accidental>, acc)
-  (boost::optional<unsigned>, octave_spec)
-  (::bmc::interval, steps)
-  (::bmc::braille::fingering_list, fingers)
-  (boost::optional<::bmc::braille::ast::tie>, tie)
-)
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::chord,
-  (::bmc::braille::ast::note, base)
-  (std::vector<::bmc::braille::ast::interval>, intervals)
-  (bool, all_tied)
-)
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::moving_note,
-  (::bmc::braille::ast::note, base)
-  (std::vector<::bmc::braille::ast::interval>, intervals)
+  by_transcriber, ambiguous_value, dots
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::value_distinction,
-  (::bmc::braille::ast::value_distinction::type, value)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::stem,
+  type, dots, tied
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::simile,
-  (boost::optional<unsigned>, octave_spec)
-  (unsigned, count)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::note,
+  articulations, acc, octave_spec, step, ambiguous_value, dots, slurs, fingers,
+  tie, extra_stems
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::measure,
-  (boost::optional<unsigned>, ending)
-  (std::vector<::bmc::braille::ast::voice>, voices)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::interval,
+  acc, octave_spec, steps, fingers, tie
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::key_and_time_signature,
-  (::bmc::key_signature, key)
-  (::bmc::time_signature, time)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::chord,
+  base, intervals, all_tied
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::measure_specification,
-  (::bmc::braille::ast::measure_specification::number_type, number)
-  (boost::optional<::bmc::braille::ast::measure_specification::number_type>, alternative)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::moving_note,
+  base, intervals
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::measure_range,
-  (::bmc::braille::ast::measure_specification, first)
-  (::bmc::braille::ast::measure_specification, last)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::value_distinction,
+  value
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::section,
-  (boost::optional<::bmc::braille::ast::key_and_time_signature>, key_and_time_sig)
-  (boost::optional<::bmc::braille::ast::section::number_type>, number)
-  (boost::optional<::bmc::braille::ast::measure_range>, range)
-  (std::vector<::bmc::braille::ast::paragraph>, paragraphs)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::simile,
+  octave_spec, count
 )
 
-BOOST_FUSION_ADAPT_STRUCT(
-  ::bmc::braille::ast::score,
-  (::bmc::key_signature, key_sig)
-  (std::vector<::bmc::time_signature>, time_sigs)
-  (std::vector<::bmc::braille::ast::part>, parts)
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::measure,
+  ending, voices
+)
+
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::key_and_time_signature,
+  key, time
+)
+
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::measure_specification,
+  number, alternative
+)
+
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::measure_range,
+  first, last
+)
+
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::section,
+  key_and_time_sig, number, range, paragraphs
+)
+
+BOOST_FUSION_ADAPT_STRUCT(::bmc::braille::ast::score,
+  key_sig, time_sigs, parts
 )
 
 #endif
