d77eaeee2362e6ea544bc76f06fb2ba2a2dfe428
Mario Lang <mlang@blind.guru>
2020-02-27 11:08:16 +0100

    Set org name and domain so settings will be saved on Windows

--- ui/qt/app.cpp
+++ ui/qt/app.cpp
@@ -9,11 +9,13 @@ int main(int argc, char *argv[])
   std::locale::global(std::locale(""));
   bmc::braille::set_default_table_from_locale();
 
-    QApplication app(argc, argv);
-    QApplication::setApplicationName("Braille Music Compiler");
-    QApplication::setApplicationVersion(BMC_VERSION);
-    BrailleMusicEditor window;
-    window.show();
+  QApplication app(argc, argv);
+  QApplication::setApplicationName("Braille Music Compiler");
+  QApplication::setApplicationVersion(BMC_VERSION);
+  QApplication::setOrganizationName("Blind Guru");
+  QApplication::setOrganizationDomain("blind.guru");
+  BrailleMusicEditor window;
+  window.show();
 
-    return app.exec();
+  return app.exec();
 }
